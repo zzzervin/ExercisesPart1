2bc621333a8bd31e897ef310bfbb160ad0e24ee6
Opilane TTHK <opilane@tthk.ee>
2021-12-07 09:37:10 +0200

    Add project files.

---
+++ ExercisesPart1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExercisesPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string fname, lname;
+            int yOb;
+            Console.WriteLine("you first name");
+
+            fname = Console.ReadLine();
+            Console.WriteLine("you last name");
+            lname = Console.ReadLine();
+
+            Console.WriteLine($"{fname}{lname}");
+
+            Console.WriteLine("enter your year and birth ");
+            yOb = Convert.ToInt32(Console.ReadLine());
+
+            int cY = Convert.ToInt32(DateTime.Now.Year.ToString());
+            int age = cY - yOb;
+
+            
+            if (age >= 13)
+            {
+                Console.WriteLine("Hello World!");
+            }
+            else
+            {
+                Console.WriteLine("No Hello World!");
+            }
+
+
+
+
+        }
+    }
+}
