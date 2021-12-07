cc70ae734a5bf98a4cee57aeb3d33d1f4aac8ab8
Ervin <opilane@HP-CZC0359NN4>
2021-12-07 09:55:33 +0200

    PersonalityTest

--- ExercisesPart1/Program.cs
+++ ExercisesPart1/Program.cs
@@ -1,41 +1,41 @@
-﻿using System;
+﻿//using System;
 
-namespace ExercisesPart1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            string fname, lname;
-            int yOb;
-            Console.WriteLine("you first name");
+//namespace ExercisesPart1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//            string fname, lname;
+//            int yOb;
+//            Console.WriteLine("you first name");
 
-            fname = Console.ReadLine();
-            Console.WriteLine("you last name");
-            lname = Console.ReadLine();
+//            fname = Console.ReadLine();
+//            Console.WriteLine("you last name");
+//            lname = Console.ReadLine();
 
-            Console.WriteLine($"{fname}{lname}");
+//            Console.WriteLine($"{fname}{lname}");
 
-            Console.WriteLine("enter your year and birth ");
-            yOb = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("enter your year and birth ");
+//            yOb = Convert.ToInt32(Console.ReadLine());
 
-            int cY = Convert.ToInt32(DateTime.Now.Year.ToString());
-            int age = cY - yOb;
-
-            
-            if (age >= 13)
-            {
-                Console.WriteLine("Hello World!");
-            }
-            else
-            {
-                Console.WriteLine("No Hello World!");
-            }
+//            int cY = Convert.ToInt32(DateTime.Now.Year.ToString());
+//            int age = cY - yOb;
 
 
+//            if (age >= 13)
+//            {
+//                Console.WriteLine("Hello World!");
+//            }
+//            else
+//            {
+//                Console.WriteLine("No Hello World!");
+//            }
 
 
-        }
-    }
-}
+
+
+//        }
+//    }
+//}
---
+++ PersonalityTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PersonalityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string color;
+
+            Console.WriteLine("you color");
+            color = Console.ReadLine().ToLower();
+
+            if (color == "red")
+            {
+                Console.WriteLine("romantic ");
+            }
+            if (color == "blue")
+            {
+                Console.WriteLine("reaibel ");
+            }
+            if (color == "yellow")
+            {
+                Console.WriteLine("cheerful ");
+            }
+            else
+            {
+                Console.WriteLine("unicorn ");
+            }
+
+        }
+    }
+}
